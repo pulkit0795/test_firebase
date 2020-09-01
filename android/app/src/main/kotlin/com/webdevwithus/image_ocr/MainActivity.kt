a3a8d59224e892ae0d6f1d62fb9853db9d3ccb7d
pulkit0795 <pulkit0795@yahoo.com>
2020-09-01 21:11:44 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/webdevwithus/image_ocr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webdevwithus.image_ocr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_ocr/test.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyApp2(),
+    );
+  }
+}
+
+class MyApp2 extends StatefulWidget {
+  @override
+  _MyApp2State createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  // bool _initialized = false;
+  // bool _error = false;
+  //
+  // // Define an async function to initialize FlutterFire
+  // // @override
+  //
+  // void initializeFlutterFire() async {
+  //   try {
+  //     // Wait for Firebase to initialize and set `_initialized` state to true
+  //     await Firebase.initializeApp();
+  //     setState(() {
+  //       _initialized = true;
+  //     });
+  //   } catch (e) {
+  //     // Set `_error` state to true if Firebase initialization fails
+  //     setState(() {
+  //       _error = true;
+  //     });
+  //   }
+  // }
+
+  // @override
+  // void initState() {
+  //   initializeFlutterFire();
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('appURL').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.data)
+              return Text(
+                'Loadingg',
+                style: TextStyle(fontSize: 30),
+              );
+            return Center(
+                child: Container(
+              child: Text(snapshot.data.documents[0]['applink']),
+            ));
+          }),
+    );
+  }
+}
+
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+//
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           // Column is also a layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Invoke "debug painting" (press "p" in the console, choose the
+//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
+//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+//           // to see the wireframe for each widget.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//             Container(),
+//             Image.asset(
+// //              'assets/edit.jpeg',
+// //              'assets/giphy.gif',
+//                 'assets/flrfile.flr'),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+//
+// class OnBoardingPage extends StatefulWidget {
+//   @override
+//   _OnBoardingPageState createState() => _OnBoardingPageState();
+// }
+//
+// class _OnBoardingPageState extends State<OnBoardingPage> {
+//   final introKey = GlobalKey<IntroductionScreenState>();
+//
+//   void _onIntroEnd(context) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute(
+//           builder: (_) => MyHomePage(title: 'Flutter Demo Home Page')),
+//     );
+//   }
+//
+//   Widget _buildImage(String assetName) {
+//     return Align(
+//       child: Image.asset('assets/$assetName.gif', width: 350.0),
+//       alignment: Alignment.bottomCenter,
+//     );
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     const bodyStyle = TextStyle(fontSize: 19.0);
+//     const pageDecoration = const PageDecoration(
+//       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+//       bodyTextStyle: bodyStyle,
+//       descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+//       pageColor: Colors.white,
+//       imagePadding: EdgeInsets.fromLTRB(0, 25.0, 0, 0),
+//     );
+//
+//     return IntroductionScreen(
+//       key: introKey,
+//       pages: [
+//         PageViewModel(
+//           title: "Fractional shares",
+//           body:
+//               "Instead of having to buy an entire share, invest any amount you want.",
+//           image: _buildImage('giphy'),
+//           decoration: pageDecoration,
+//         ),
+//       ],
+//       onDone: () => _onIntroEnd(context),
+//       onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+//       showSkipButton: true,
+//       skipFlex: 0,
+//       nextFlex: 0,
+//       skip: const Text('Skip'),
+//       next: const Icon(Icons.arrow_forward),
+//       done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+//       dotsDecorator: const DotsDecorator(
+//         size: Size(10.0, 10.0),
+//         color: Color(0xFFBDBDBD),
+//         activeSize: Size(22.0, 10.0),
+//         activeShape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(25.0)),
+//         ),
+//       ),
+//     );
+//   }
+// }
